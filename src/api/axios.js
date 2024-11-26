ee3152e6e53dbbe2e77b9ddc2d3e00400cac6be9
hanbyeol <leehanbyeol971219@gmail.com>
2024-11-26 15:54:22 +0900

    axios url 수정

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const axiosInstance = axios.create({
-  baseURL: process.env.DEPLOY_URL || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_DEPLOY_URL || "http://localhost:5000",
   headers: { "Content-Type": "application/json" },
 });
 
