c4846308478e18499b09dddf6659da5328d1553c
hanbyeol <leehanbyeol971219@gmail.com>
2024-11-28 18:52:28 +0900

    axios.js 배포 환경에서의 base URL 수정

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "/sendMessage"
+      ? "/api/proxy"
       : "http://localhost:5000",
   headers: { "Content-Type": "application/json" },
 });
