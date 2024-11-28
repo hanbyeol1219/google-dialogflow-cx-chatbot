df6108a2f729a9b6a210d837d8a4b86f44a1c65f
hanbyeol <leehanbyeol971219@gmail.com>
2024-11-28 19:30:01 +0900

    vercel router 경로 수정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,12 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Chat } from "./../pages/Chat";
 
 export const Router = () => {
-  const isProduction = process.env.NODE_ENV === "production";
-
   return (
-    <BrowserRouter
-      basename={isProduction ? "/google-dialogflow-cx-chatbot" : "/"}
-    >
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Chat />} />
         <Route path="/:name" element={<Chat />} />
