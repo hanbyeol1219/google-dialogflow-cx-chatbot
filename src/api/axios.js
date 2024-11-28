1c2dcd7659864075282e0292f540bd734c4bf245
hanbyeol <leehanbyeol971219@gmail.com>
2024-11-28 19:04:01 +0900

    axios base url 수정

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,9 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL:
-    process.env.NODE_ENV === "production"
-      ? "/sendMessage"
-      : "http://localhost:5000",
+    process.env.NODE_ENV === "production" ? "/api" : "http://localhost:5000",
   headers: { "Content-Type": "application/json" },
 });
 
