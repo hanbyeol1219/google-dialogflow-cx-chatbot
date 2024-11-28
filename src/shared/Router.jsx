0392479ae14ed777d110b57e605fac33dd9fba28
hanbyeol <leehanbyeol971219@gmail.com>
2024-11-28 19:27:50 +0900

    vercel 배포 환경에서의 router 설정 수정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,8 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Chat } from "./../pages/Chat";
 
 export const Router = () => {
+  const isProduction = process.env.NODE_ENV === "production";
+
   return (
-    <BrowserRouter>
+    <BrowserRouter
+      basename={isProduction ? "/google-dialogflow-cx-chatbot" : "/"}
+    >
       <Routes>
         <Route path="/" element={<Chat />} />
         <Route path="/:name" element={<Chat />} />
